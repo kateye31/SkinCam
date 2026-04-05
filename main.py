906e150bd16ab716fcca3a8467a87f9ff1ece979
Jay <nelsonjahmeelay@gmail.com>
2026-04-04 23:21:21 -0400

    Implement file upload and analysis endpoint in main.py; add .env for API key

--- main.py
+++ main.py
@@ -1,12 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask,render_template, request, redirect, jsonify
+from dotenv import load_dotenv
+import requests
+import base64
+import os
 
+
+load_dotenv()
 
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+@app.route('/analyze', methods=['POST'])
+def upload_file():
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({'error':'no file'})
+        file = request.files['file']
+
+        if file.filename == ' ':
+            return jsonify({'error': 'no file'})
+        image_data = base64.b64encode(file.read()).decode('utf-8')
+
+        api_key = os.getenv('ANTHROPIC_API_KEY') 
+
+        headers = {
+        "x-api-key" : api_key ,
+        "anthropic-version" : "2023-06-01" ,
+        "content-type" : "application/json" ,
+        }
+
+        body = {
+        "model": "claude-haiku-4-5",
+        "max_tokens" : 500,
+        "messages": [
+            {"role": "user", "content":"Hello, Claude"}
+        ]
+        }
+
+        response = requests.post('https://api.anthropic.com/v1/messages', headers=headers, json=body)
+        
+        return jsonify(response.json())
+        
+        
 
 if __name__ == '__main__':
+
     app.run(debug=True)
